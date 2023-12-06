f7c9948345e4e6928125dca213a8e933072eef7f
Adham2023 <adham.muhammadjonov1923@gmail.com>
2023-12-06 05:26:59 +0500

    done

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -6,7 +6,7 @@ const auth = useAuthStore()
   <div w-full b-b-1 b-b-zinc-300 dark:b-b-zinc-700 class="z-20 h-4rem w-100% bg-white bg-opacity-70 px-1em backdrop-blur-lg dark:text-white" p="~ y-5">
     <div flex="~ gap-5rem" mx-auto h-full items-center justify-between px-2 container>
       <Logo />
-      <pre>
+      <pre v-if="auth.user">
         {{ auth.user._id }} / {{ auth.user.username }}
       </pre>
       <ul flex="~ gap-1rem" font-500>
